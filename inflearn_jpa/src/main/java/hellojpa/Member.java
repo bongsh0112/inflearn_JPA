ff6927f2cac409a24c48d790775a4b302fdda9f3
bongsh0112 <bongsh0112@gmail.com>
2023-06-08 23:21:19 +0900

    기본 키 매핑 GeneratedValue = TABLE 확인

--- inflearn_jpa/src/main/java/hellojpa/Member.java
+++ inflearn_jpa/src/main/java/hellojpa/Member.java
@@ -4,12 +4,15 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 //@Table(name = "MEMBER")
 public class Member {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq")
+  @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
   private Long id;
   @Column(name = "name", nullable = false)
   private String username;
