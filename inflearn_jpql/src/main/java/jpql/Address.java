9b36db3c0d567d09da081f1004d468415c8a3c83
bongsh0112 <bongsh0112@gmail.com>
2023-06-16 00:23:21 +0900

    JPQL 기본 설정

---
+++ inflearn_jpql/src/main/java/jpql/Address.java
@@ -0,0 +1,26 @@
+package jpql;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+  private String city;
+  private String street;
+  private String zipcode;
+
+  public Address() {
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+}
