f47bee3c6f3c0dc9864ce0a4d9b6509f95071d89
bongsh0112 <bongsh0112@gmail.com>
2023-06-18 00:23:56 +0900

    fetch join 알아보기

--- inflearn_jpql/src/main/java/jpql/Main.java
+++ inflearn_jpql/src/main/java/jpql/Main.java
@@ -14,33 +14,46 @@ public class Main {
 
     try {
 
-      Team team = new Team();
-      em.persist(team);
+      Team teamA = new Team();
+      teamA.setName("teamA");
+      em.persist(teamA);
 
-      Member member = new Member();
-      member.setUsername("관리자1");
-      member.setTeam(team);
-      em.persist(member);
+      Team teamB = new Team();
+      teamB.setName("teamB");
+      em.persist(teamB);
+
+      Member member1 = new Member();
+      member1.setUsername("member1");
+      member1.setTeam(teamA);
+      em.persist(member1);
 
       Member member2 = new Member();
-      member2.setUsername("관리자2");
-      member.setTeam(team);
+      member2.setUsername("member2");
+      member2.setTeam(teamA);
       em.persist(member2);
+
+      Member member3 = new Member();
+      member3.setUsername("member3");
+      member3.setTeam(teamB);
+      em.persist(member3);
 
       em.flush();
       em.clear();
 
-      String query = "select m.username from Member m"; // 경로 표현식 사용 -> 상태 필드 사용
-//      String query = "select m.team From Member m"; // 경로 표현식 사용 시 주의 -> 단일 값 연관 경로 조회 시 묵시적 내부 조인 발생
-      /* String query = "select t.members.username From Team t"; // 경로 표현식 사용 시 주의 -> 컬렉션 값 연관 경로 조회 시 묵시적 내부 조인 발생. 이 코드는 에러발생
-      Collection result = em.createQuery(query, Collection.class).getResultList(); */
-//      String query = "select t.members from Team t join t.members m"; // 명시적 조인을 통한 조회
+//      String query = "select m from Member m"; // fetch join 사용 전
+      String query = "select m from Member m join fetch m.team"; // fetch join 사용시 -> 팀은 프록시가 아니라 실제 Team을 가져옴!
 
-      List<Team> result = em.createQuery(query, Team.class)
-                    .getResultList();
+      List<Member> result = em.createQuery(query, Member.class)
+              .getResultList();
 
-      for(Team m : result) {
-        System.out.println("s = " + m);
+      for(Member m : result) {
+        System.out.println("m = " + m.getUsername() + " " + m.getTeam().getName());
+        // Team은 LAZY에 의해 프록시로 들어오게 되고 결국 지연 로딩. 실제로 getName을 호출할 때 마다 DB에 쿼리를 날림.
+        // member1, TeamA(SQL)
+        // member2, TeamA(1차캐시)
+        // member3, TeamB(SQL)
+
+        // 만약 회원이 100명이라면 최악의 경우 쿼리가 100번이 나갈 것.. N + 1
       }
       tx.commit();
     } catch (Exception e) {
