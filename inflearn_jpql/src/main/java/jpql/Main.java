61b4b65f094e94f2d5d044307722a091e9df519b
bongsh0112 <bongsh0112@gmail.com>
2023-06-18 01:37:02 +0900

    벌크 연산 사용

--- inflearn_jpql/src/main/java/jpql/Main.java
+++ inflearn_jpql/src/main/java/jpql/Main.java
@@ -37,16 +37,19 @@ public class Main {
       member3.setTeam(teamB);
       em.persist(member3);
 
-      em.flush();
-      em.clear();
+//      em.flush();
+//      em.clear();
 
-      List<Member> findMember = em.createNamedQuery("Member.findByUserName", Member.class)
-              .setParameter("username", member1)
-              .getResultList(); // NamedQuery 사용
-      
-      for (Member m : findMember) {
-        System.out.println("m = " + m);
-      }
+      // flush 자동 호출함! 왜냐면 쿼리가 나갈 때 flush는 자동 호출되기 때문에!
+      int resultCount = em.createQuery("update Member m set m.age = 20")
+              .executeUpdate();
+
+      em.clear(); // 영속성 컨텍스트를 초기화하면
+
+      Member findMember = em.find(Member.class, member1.getId()); // find는 영속성 컨텍스트를 먼저 들여다보는데 없다! 그러면 DB로 가서 찾아옴
+
+      System.out.println("resultCount = " + resultCount);
+      System.out.println("findMember.getUsername() = " + findMember.getAge()); // 그러면 벌크 연산으로 인해 바뀐 DB의 데이터가 성공적으로 찾아짐
 
       tx.commit();
     } catch (Exception e) {
