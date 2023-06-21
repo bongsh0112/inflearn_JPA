cd48c6d146176413f9a8da9dd5ee01a8ba854307
bongsh0112 <bongsh0112@gmail.com>
2023-06-22 01:46:29 +0900

    :technologist: XToOne의 지연 로딩, cascade 전략, 컬렉션 필드 초기화 - 엔티티 개발 시 주의점

--- inflearn_jpaApp/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ inflearn_jpaApp/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -24,4 +24,5 @@ public class Member {
   
   @OneToMany(mappedBy = "member")
   private List<Order> orders = new ArrayList<>(); // 여기에 뭔가 값을 넣는다고 해도 fk값이 바뀌지 않는다.
+  // nullPointerException을 방지함.
 }
