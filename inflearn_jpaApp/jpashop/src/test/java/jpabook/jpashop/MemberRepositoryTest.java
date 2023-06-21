bd7f95140507e8732551b25465f49ea960d267f1
bongsh0112 <bongsh0112@gmail.com>
2023-06-22 00:07:23 +0900

    test 맛보기

--- inflearn_jpaApp/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ inflearn_jpaApp/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-import org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
