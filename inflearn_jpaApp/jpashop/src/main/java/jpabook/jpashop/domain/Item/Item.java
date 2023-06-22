40aeee964fcc35865ce49349d428ffdb57fc0236
bongsh0112 <bongsh0112@gmail.com>
2023-06-22 09:19:24 +0900

    상품 엔티티 개발 - Item 비즈니스 로직 추가

--- inflearn_jpaApp/jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ inflearn_jpaApp/jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.Item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,17 @@ public abstract class Item {
   
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
+  
+  // 비즈니스 로직 - Setter를 가지고 값을 변경하는 것이 아니라 비즈니스 로직으로 변경!! //
+  public void addStock(int quantity){
+    this.stockQuantity += quantity;
+  }
+  
+  public void removeStock(int quantity) throws NotEnoughStockException {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
