35f2eb6e90c0e4464d8f2a561a0e83c945c9b953
bongsh0112 <bongsh0112@gmail.com>
2023-07-05 20:03:36 +0900

    :technologist: Hibernate5ModuleJakarta로 벽여

--- inflearn_jpaApp/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ inflearn_jpaApp/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,6 +1,6 @@
 package jpabook.jpashop;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +13,8 @@ public class JpashopApplication {
 	}
 	
 	@Bean
-	Hibernate5Module hibernate5Module() {
-		Hibernate5Module hibernate5Module = new Hibernate5Module();
-		return hibernate5Module;
+	Hibernate5JakartaModule hibernate5Module() {
+		return new Hibernate5JakartaModule();
 	}
 
 }
